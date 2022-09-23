d32ebd258578682dccd3c2974441bc8d7ed44166
seanvd42 <sean.j.van.duser@gmail.com>
2022-09-23 10:05:36 -0400

    Moved view object around and modernized code

---
+++ TrelloToMicrosoftPlannerMigrator/Models/UserInput.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Graph;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.PortableExecutable;
+using TrelloToMicrosoftPlannerMigrator.Models.TrelloBoardSubModels;
+using TrelloToMicrosoftPlannerMigrator.Services;
+
+namespace TrelloToMicrosoftPlannerMigrator.Models
+{
+    public class UserInput
+    {
+        [Display(Name = "Trello Board File")]
+        public IFormFile? TrelloBoardFile { get; set; }
+
+        [Display(Name = "Include archived lists?")]
+        public bool IncludeArchivedLists { get; set; }
+
+        [Display(Name = "Include archived cards?")]
+        public bool IncludeArchivedCards { get; set; }
+
+        public string TestDisplay { get; set; }
+    }
+}
